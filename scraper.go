70c8672b938a358e6d71ccb48e03af79980b02dd
duolok <dusan.lecic5@gmail.com>
2024-08-04 22:44:57 +0200

    add: default colly callbacks

--- scraper.go
+++ scraper.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "log"
     "github.com/gocolly/colly"
 )
 
@@ -10,5 +11,32 @@ func main() {
     c := colly.NewCollector(colly.AllowedDomains("www.allkeyshop.com"))
     fmt.Println(c.Visit("www.allkeyshop.com"))
 
+    c.OnRequest(func(r *colly.Request) {
+        fmt.Println("Visiting", r.URL)
+    })
+
+    c.OnError(func(_ *colly.Response, err error) {
+        log.Println("Something went wrong: ", err)
+    })
+
+    c.OnResponse(func(r *colly.Response) {
+        fmt.Println("Visited", r.Request.URL)
+    })
+
+    c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+        e.Request.Visit(e.Attr("href"))
+    })
+
+    c.OnHTML("tr td:nth-of-type(1)", func(e *colly.HTMLElement) {
+        fmt.Println("First column of a table row:", e.Text)
+    })
+
+    c.OnXML("//h1", func(e *colly.XMLElement) {
+        fmt.Println(e.Text)
+    })
+
+    c.OnScraped(func(r *colly.Response) {
+        fmt.Println("Finished", r.Request.URL)
+    })
 }
 
