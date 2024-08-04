3203d11271a3eae25e1fa20d460ba5741941aeb0
duolok <dusan.lecic5@gmail.com>
2024-08-04 23:07:42 +0200

    refactor: code formatting

--- scraper.go
+++ scraper.go
@@ -1,11 +1,11 @@
 package main
 
 import (
- "encoding/csv"
- "log"
- "os"
- "time"
- "github.com/gocolly/colly"
+    "encoding/csv"
+    "log"
+    "os"
+    "time"
+    "github.com/gocolly/colly"
 )
 
 type gameStruct struct {
@@ -15,64 +15,63 @@ type gameStruct struct {
 }
 
 func scrapeAndWriteCSV() []gameStruct {
- var scrapData []gameStruct
+    var scrapData []gameStruct
 
- c := colly.NewCollector(
-  colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"),
- )
+    c := colly.NewCollector(
+        colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"),
+        )
 
     c.SetRequestTimeout(time.Second * 10)
 
- c.OnHTML(".search-results-row", func(e *colly.HTMLElement) {
-  game := gameStruct{}
+    c.OnHTML(".search-results-row", func(e *colly.HTMLElement) {
+        game := gameStruct{}
 
-  game.title = e.ChildText(".search-results-row-game-title")
-  game.description = e.ChildText(".search-results-row-game-infos")
-  game.price = e.ChildText(".search-results-row-price")
+        game.title = e.ChildText(".search-results-row-game-title")
+        game.description = e.ChildText(".search-results-row-game-infos")
+        game.price = e.ChildText(".search-results-row-price")
 
-  scrapData = append(scrapData, game)
- })
+        scrapData = append(scrapData, game)
+    })
 
- c.OnError(func(r *colly.Response, err error) {
-  log.Printf("Request URL: %s failed with response: %v\n", r.Request.URL, err)
- })
+    c.OnError(func(r *colly.Response, err error) {
+        log.Printf("Request URL: %s failed with response: %v\n", r.Request.URL, err)
+    })
 
- c.Visit("https://www.allkeyshop.com/blog/catalogue/search-Elden+Ring/")
- c.Wait()
+    c.Visit("https://www.allkeyshop.com/blog/catalogue/search-Elden+Ring/")
+    c.Wait()
 
- file, err := os.Create("link1.csv")
- if err != nil {
-  log.Fatalln("Failed to create output CSV file", err)
- }
- defer file.Close()
+    file, err := os.Create("link1.csv")
+    if err != nil {
+        log.Fatalln("Failed to create output CSV file", err)
+    }
+    defer file.Close()
 
- writer := csv.NewWriter(file)
+    writer := csv.NewWriter(file)
 
- headers := []string{
-  "url",
-  "image",
-  "title",
-  "text",
- }
- writer.Write(headers)
+    headers := []string{
+        "url",
+        "image",
+        "title",
+        "text",
+    }
+    writer.Write(headers)
 
- for _, data := range scrapData {
-  // Converting a data to an array of strings
-  record := []string{
-   data.title,
-   data.description,
-   data.price,
-  }
-  writer.Write(record)
- }
+    for _, data := range scrapData {
+        record := []string{
+            data.title,
+            data.description,
+            data.price,
+        }
+        writer.Write(record)
+    }
 
- writer.Flush()
+    writer.Flush()
 
- if err := writer.Error(); err != nil {
-  log.Fatalln("Error writing CSV:", err)
- }
+    if err := writer.Error(); err != nil {
+        log.Fatalln("Error writing CSV:", err)
+    }
 
- return scrapData
+    return scrapData
 }
 
 func main() {
