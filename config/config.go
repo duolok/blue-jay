1ef0e0e5fce527b4e6af0bf95138afb3c16aa108
duolok <dusan.lecic5@gmail.com>
2024-08-05 15:33:37 +0200

    add: game price variable

--- config/config.go
+++ config/config.go
@@ -16,6 +16,7 @@ func LoadConfig() (*Config, error) {
     viper.SetDefault("USER_AGENT", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
     viper.SetDefault("REQUEST_TIMEOUT", 10*time.Second)
     viper.SetDefault("GAME_LIST_URL", "https://www.allkeyshop.com/blog/catalogue/search-")
+    viper.SetDefault("GAME_PRICE_URL", "https://www.allkeyshop.com/blog/buy-" )
     viper.SetDefault("CSV_FILE_NAME", "games.csv")
 
     viper.AutomaticEnv()
@@ -34,6 +35,7 @@ func LoadConfig() (*Config, error) {
         UserAgent:      viper.GetString("USER_AGENT"),
         RequestTimeout: viper.GetDuration("REQUEST_TIMEOUT"),
         GameListURL:    viper.GetString("GAME_LIST_URL"),
+        GamePriceURL:   viper.GetString("GAME_PRICE_URL"),
         CSVFileName:    viper.GetString("CSV_FILE_NAME"),
     }
 
