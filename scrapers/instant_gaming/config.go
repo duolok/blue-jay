3f01c9dcc81765ccf4ca42c2edb5caef1beb7828
duolok <dusan.lecic5@gmail.com>
2024-08-06 12:51:32 +0200

    add: instant gaming scraper

---
+++ scrapers/instant_gaming/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+    "time"
+    "github.com/spf13/viper"
+)
+
+type Config struct {
+    UserAgent        string
+    RequestTimeout   time.Duration
+    GameListURL      string
+    GamePriceURL     string
+    CSVFileName      string
+}
+
+func LoadConfig() (*Config, error) {
+    viper.SetDefault("USER_AGENT", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+    viper.SetDefault("REQUEST_TIMEOUT", 10*time.Second)
+    viper.SetDefault("GAME_LIST_URL", "https://www.instant-gaming.com/en/search/?query=")
+    viper.SetDefault("GAME_PRICE_URL", "https://www.allkeyshop.com/blog/buy-" )
+    viper.SetDefault("CSV_FILE_NAME", "games.csv")
+
+    viper.AutomaticEnv()
+
+    viper.SetConfigName("config")
+    viper.SetConfigType("yaml")
+    viper.AddConfigPath(".")
+
+    if err := viper.ReadInConfig(); err != nil {
+        if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+            return nil, err
+        }
+    }
+
+    config := &Config{
+        UserAgent:      viper.GetString("USER_AGENT"),
+        RequestTimeout: viper.GetDuration("REQUEST_TIMEOUT"),
+        GameListURL:    viper.GetString("GAME_LIST_URL"),
+        GamePriceURL:   viper.GetString("GAME_PRICE_URL"),
+        CSVFileName:    viper.GetString("CSV_FILE_NAME"),
+    }
+
+    return config, nil
+}
+
---
+++ scrapers/instant_gaming/scraper.go
@@ -0,0 +1,93 @@
+package instant_gaming
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/duolok/blue-jay/scrapers/instant-gaming/config"
+	"github.com/duolok/blue-jay/interfaces"
+	"github.com/gocolly/colly"
+)
+
+type InstantGamingScraper struct {
+	Config *config.Config
+	Games  []Game
+}
+
+type Game struct {
+	Title string
+	Link  string
+	Price string
+}
+
+func NewInstantGamingScraper(cfg *config.Config) *InstantGamingScraper {
+	return &InstantGamingScraper{
+		Config: cfg,
+	}
+}
+
+func (s *InstantGamingScraper) Scrape(gameName string) {
+	collector := colly.NewCollector(
+		colly.UserAgent(s.Config.UserAgent),
+		colly.Async(true),
+	)
+
+	collector.SetRequestTimeout(time.Duration(s.Config.RequestTimeout) * time.Second)
+	collector.OnHTML(".search .item", func(h *colly.HTMLElement) {
+		game := Game{
+			Title: h.ChildText(".title"),
+			Link:  h.ChildAttr("a", "href"),
+			Price: h.ChildText(".price"),
+		}
+
+		if game.Title != "" {
+			s.Games = append(s.Games, game)
+		}
+	})
+
+	collector.OnError(func(r *colly.Response, err error) {
+		log.Printf("Request URL: %s failed with response: %v\n", r.Request.URL, err)
+	})
+
+	err := collector.Visit(s.Config.GameListURL + transformGameSearchString(gameName))
+	if err != nil {
+		log.Fatalf("Failed to visit URL %s: %v", s.Config.GameListURL, err)
+	}
+
+	collector.Wait()
+}
+
+func (s *InstantGamingScraper) WriteToFile(path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+
+	headers := []string{"Title", "Price", "Link"}
+	if err := writer.Write(headers); err != nil {
+		return err
+	}
+
+	for _, game := range s.Games {
+		record := []string{game.Title, game.Price, game.Link}
+		if err := writer.Write(record); err != nil {
+			return err
+		}
+	}
+	writer.Flush()
+	return writer.Error()
+}
+
+func (s *InstantGamingScraper) GetPrices() {
+}
+
+func transformGameSearchString(input string) string {
+	return  strings.ReplaceAll(input, " ", "%20")
+}
+
