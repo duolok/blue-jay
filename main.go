8db8ed8899046e9d8d7468718b6f92c1f27a7c17
duolok <dusan.lecic5@gmail.com>
2024-08-06 14:25:53 +0200

    add: load all scrapers method

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"log"
 
 	"github.com/duolok/blue-jay/scrapers/instant_gaming"
 )
 
+func loadScrapers() []string {
+	var scrapers []string
+
+	items, _ := os.ReadDir("./scrapers/")
+	for _, item := range items {
+		if item.IsDir() {
+			scrapers = append(scrapers, item.Name())
+		}
+	}
+
+	return scrapers
+}
+
+
 func main() {
+	scrapers := loadScrapers()
+	for _, item := range scrapers {
+		fmt.Println(item)
+	}
+
+
 	cfg, err := instant_gaming.LoadConfig()
 	if err != nil {
 		log.Fatalf("Failed to load configuration: %v", err)
