b16cc1bee3cac72a29b58cdc020fca2e1ab8cdc6
duolok <dusan.lecic5@gmail.com>
2024-08-06 14:51:55 +0200

    add: load last search

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"log"
+	"bufio"
 
 	"github.com/duolok/blue-jay/scrapers/instant_gaming"
 )
@@ -22,10 +23,41 @@ func loadScrapers() []string {
 }
 
 
+func loadLastSearch(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lines := []string{}
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text()) 
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading file: %w", err)
+	}
+
+	return lines, nil
+}
+
 func main() {
 	scrapers := loadScrapers()
 	for _, item := range scrapers {
 		fmt.Println(item)
+	}
+
+	lines, err := loadLastSearch("games.csv")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return 
+	}
+
+	for _, line := range(lines) {
+		fmt.Println(line)
 	}
 
 
